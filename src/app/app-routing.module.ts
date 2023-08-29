3c802770c1b547ceeb0ce5f4c2d75b9095a45f05
Volodymyr_Kolyada <90142111+volodymyr1402@users.noreply.github.com>
2023-08-29 15:35:20 +0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,26 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductPageComponent } from './pages/product-page/product-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SmartphonesComponent } from './pages/smartphones/smartphones.component';
+import { LaptopsComponent } from './pages/laptops/laptops.component';
+import { FragrancesComponent } from './pages/fragrances/fragrances.component';
+import { SkincareComponent } from './pages/skincare/skincare.component';
+import { GroceriesComponent } from './pages/groceries/groceries.component';
+import { HomeDecorationComponent } from './pages/home-decoration/home-decoration.component';
 
 const routes: Routes = [
-  {path:'', component:ProductPageComponent},
-  {path:'about', component: AboutPageComponent}
+  { path: '', component: ProductPageComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: 'smartphones', component: SmartphonesComponent },
+  { path: 'laptops', component: LaptopsComponent },
+  { path: 'fragrances', component: FragrancesComponent },
+  { path: 'skincare', component: SkincareComponent },
+  { path: 'groceries', component: GroceriesComponent },
+  { path: 'home-decoration', component: HomeDecorationComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
